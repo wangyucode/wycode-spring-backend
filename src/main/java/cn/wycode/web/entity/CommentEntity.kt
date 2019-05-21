fd4288fc20866706f1d89e3669d51b1b9956500d
wayne <wangyu@wycode.cn>
2019-05-21 19:21:23 +0800

    添加评论表

--- src/main/java/cn/wycode/web/entity/CommentEntity.kt
+++ src/main/java/cn/wycode/web/entity/CommentEntity.kt
@@ -1,22 +1,34 @@
 package cn.wycode.web.entity
 
+import io.swagger.annotations.ApiModelProperty
 import javax.persistence.*
 
-//@Entity
-//data class Comment(
-//        @Id
-//        @GeneratedValue(generator = "seq_comment")
-//        @SequenceGenerator(name = "seq_comment", sequenceName = "SEQ_COMMENT", allocationSize = 1, initialValue = 1)
-//        val id: Long? = null,
-//        val appId: String = "",
-//        @Column(length = 1023)
-//        val content: String? = null,
-//        val type: Int = 0, //1、文字回复；2、点赞；3、图片回复
-//        val fromUserId: String = "",
-//        val fromUserName: String = "匿名用户",
-//        val fromUserIcon: String = "",
-//        val toUserId: String? = null,
-//        val toUserName: String? = "匿名用户",
-//        val toUserIcon: String? = "",
-//        var likeCount: Int = 0,
-//        var unlikeCount: Int = 0)
+@Entity
+data class Comment(
+        @Id
+        @GeneratedValue(generator = "seq_comment")
+        @SequenceGenerator(name = "seq_comment", sequenceName = "SEQ_COMMENT", allocationSize = 1, initialValue = 1)
+        val id: Long = 0,
+        @ApiModelProperty(value = "主题id")
+        val subjectId: String = "",
+        @ManyToOne
+        val app: CommentApp = CommentApp(),
+        @Column(length = 1023)
+        val content: String? = null,
+        @ApiModelProperty(value = "评论类型，1.文字评论，2.点赞，3.图片评论")
+        val type: Int = 1,
+        val fromUserId: String = "",
+        val fromUserName: String = "匿名用户",
+        val fromUserIcon: String = "",
+        val toUserId: String? = null,
+        val toUserName: String? = null,
+        val toUserIcon: String? = null,
+        val toContent: String? = null,
+        val likeCount: Int = 0)
+
+@Entity
+data class CommentApp(
+        @Id
+        val name:String = "",
+        val accessKey: String = ""
+)
