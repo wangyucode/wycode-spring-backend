6b6bb649851e2f6b378f12e63e82c86b5b9a0ddb
wayne <wangyu@wycode.cn>
2019-07-29 18:06:09 +0800

    cors

--- src/main/java/cn/wycode/web/config/MvcConfig.kt
+++ src/main/java/cn/wycode/web/config/MvcConfig.kt
@@ -1,14 +1,29 @@
 package cn.wycode.web.config
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.core.Ordered
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Configuration
-class MvcConfig:WebMvcConfigurer {
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/api/**")
-        registry.addMapping("/v2/api-docs") // TODO  this does not work
+@Configuration
+class MvcConfig : WebMvcConfigurer {
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.allowedMethods = listOf("POST", "OPTIONS", "GET", "DELETE", "PUT")
+        source.registerCorsConfiguration("/api/**", config)
+        source.registerCorsConfiguration("/v2/api-docs", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
     }
 }
